afccc8954fd3e992923cf9eff4480459da4f4feb
claudiasgithub2 <138537469+claudiasgithub2@users.noreply.github.com>
2023-08-03 15:53:37 +0000

    cartaaleatoria pronta

--- src/app.js
+++ src/app.js
@@ -5,7 +5,41 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+//write your code here
+
+window.onload = function cartasAleatorias() {
+  let palos = ["♦", "♥", "♠", "♣"];
+  let numeros = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let palosIndex = palos[Math.floor(Math.random() * palos.length)];
+  let numerosIndex = numeros[Math.floor(Math.random() * numeros.length)];
+  console.log(palosIndex, numerosIndex);
+  document.getElementById("numero").innerHTML = numerosIndex;
+  document.getElementById("palo").innerHTML = palosIndex;
+  document.getElementById("palo2").innerHTML = palosIndex;
+
+  // cambiar color palo y palos
+
+  if (palosIndex == "♦" || palosIndex == "♥") {
+    // console.log("palo");
+    document.getElementById("palo").className =
+      "card-title text-start text-danger";
+    document.getElementById("palo2").className =
+      "card-title text-end text-danger";
+    document.getElementById("numero").className =
+      "card-title text-center text-danger";
+  }
 };
